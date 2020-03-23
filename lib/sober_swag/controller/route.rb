9a548c7c036b4b0cee8b334789888badb4b07cf8
Anthony Super <asuper@sondermind.com>
2020-03-23 11:23:16 -0600

    Param/Query types on actions

--- lib/sober_swag/controller/route.rb
+++ lib/sober_swag/controller/route.rb
@@ -14,9 +14,18 @@ module SoberSwag
       attr_reader :controller, :method, :path, :action_name, :body_class
 
       def body(&block)
-        @body_class = Class.new(Dry::Struct, &block)
-        @body_class.transform_keys(&:to_sym)
+        @body_class = make_struct!(&block)
         action_module.const_set('Body', @body_class)
+      end
+
+      def query(&block)
+        @query_class = make_struct!(&block)
+        action_module.const_set('Query', @query_class)
+      end
+
+      def path_params(&block)
+        @path_params_class = make_struct!(&block)
+        action_module.const_set('PathParams', @path_params_class)
       end
 
       def action(&body)
@@ -29,6 +38,10 @@ module SoberSwag
 
       def assign_action_module!
         @controller.send(:const_set, action_name.to_s.classify, action_module)
+      end
+
+      def make_struct!(&block)
+        Class.new(Dry::Struct, &block).tap { |e| e.transform_keys(&:to_sym) }
       end
 
       def action_module
