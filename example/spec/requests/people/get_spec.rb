c2456e05f2c6449a6b5f01ef6bf39dca3108322d
Anthony Super <asuper@sondermind.com>
2020-07-16 14:21:56 -0600

    Specs for example app (#15)
    * Fixup example
    
    * Remove legacy tests
    
    * Install rspec
    
    * Spec for people controller
    
    * Run example specs in CI
    
    * Use ruby 2.7.1
    
    It's good to be ahead.
    
    * Cache example deps
    
    Installing nokogiri takes way too long.
    
    * Bad request specs/handling
    
    Controllers now rescue_from Dry::Struct::Error s
    
    * Request specs for create
    
    I'll admit I'm missing my custom matchers from my day job.
    
    * Specs for get actions

---
+++ example/spec/requests/people/get_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Getting a person' do
+  context 'with a good id' do
+    let!(:person) { Person.create(first_name: 'Anthony', last_name: 'Guy') }
+    let(:request) { get "/people/#{person.id}" }
+
+    describe 'the response' do
+      subject { request && response }
+      it { should be_successful }
+      it { should_not be_server_error }
+    end
+
+    describe 'the response body' do
+      subject { request && response && JSON.parse(response.body) }
+
+      it { should include('first_name' => 'Anthony') }
+      it { should include('last_name' => 'Guy') }
+    end
+  end
+
+  context 'with a bad id' do
+    let(:request) { get '/people/my-awesome-guy' }
+
+    describe 'the response' do
+      subject { request && response }
+      it { should_not be_successful }
+      it { should_not be_server_error }
+      it { should have_http_status(400) }
+      it { should be_bad_request }
+    end
+  end
+end
