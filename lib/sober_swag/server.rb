a7a98e33af09063d0339926c78462f026de2a48c
Anthony Super <asuper@sondermind.com>
2021-03-10 15:04:00 -0700

    Use Redoc for Documentation UI (#72)
    * Use Redoc for Documentation UI
    
    This makes the generated UI much nicer-looking.
    
    * Allow setting of redoc version
    
    This is just a config you pass into the server before you mount it.
    
    * Add YARD for redoc version

--- lib/sober_swag/server.rb
+++ lib/sober_swag/server.rb
@@ -22,28 +22,40 @@ module SoberSwag
     #
     # @param controller_proc [Proc] a proc that, when called, gives a list of {SoberSwag::Controller}s to document
     # @param cache [Bool | Proc] if we should cache our defintions (default false)
+    # @param redoc_version [String] what version of the redoc library to use to display UI (default 'next', the latest version).
     def initialize(
       controller_proc: RAILS_CONTROLLER_PROC,
-      cache: false
+      cache: false,
+      redoc_version: 'next'
     )
       @controller_proc = controller_proc
       @cache = cache
+      @html = EFFECT_HTML.gsub(/REDOC_VERSION/, redoc_version)
     end
 
     EFFECT_HTML = <<~HTML.freeze
       <!DOCTYPE html>
       <html>
         <head>
-          <title>Swagger-UI</title>
-          <script src="https://unpkg.com/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
-          <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@3.23.4/swagger-ui.css"></link>
+          <title>ReDoc</title>
+          <!-- needed for adaptive design -->
+          <meta charset="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
+
+          <!--
+          ReDoc doesn't change outer page styles
+          -->
+          <style>
+            body {
+              margin: 0;
+              padding: 0;
+            }
+          </style>
         </head>
         <body>
-          <div id="swagger">
-          </div>
-          <script>
-            SwaggerUIBundle({url: 'SCRIPT_NAME', dom_id: '#swagger'})
-          </script>
+          <redoc spec-url='SCRIPT_NAME'></redoc>
+          <script src="https://cdn.jsdelivr.net/npm/redoc@REDOC_VERSION/bundles/redoc.standalone.js"> </script>
         </body>
       </html>
     HTML
@@ -53,7 +65,7 @@ module SoberSwag
       if req.path_info&.match?(/json/si) || req.get_header('Accept')&.match?(/json/si)
         [200, { 'Content-Type' => 'application/json' }, [generate_json_string]]
       else
-        [200, { 'Content-Type' => 'text/html' }, [EFFECT_HTML.gsub(/SCRIPT_NAME/, env['SCRIPT_NAME'] + '.json')]]
+        [200, { 'Content-Type' => 'text/html' }, [@html.gsub(/SCRIPT_NAME/, "#{env['SCRIPT_NAME']}.json")]]
       end
     end
 
