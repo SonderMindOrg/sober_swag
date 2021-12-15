5ca038f12f0545b21ea1e954c253c0628c820568
Anthony Super <asuper@sondermind.com>
2021-12-15 13:46:55 -0700

    [REV] Clarify that ViaMap is Contravariant

--- lib/sober_swag/reporting/output/via_map.rb
+++ lib/sober_swag/reporting/output/via_map.rb
@@ -4,6 +4,30 @@ module SoberSwag
       ##
       # Apply a mapping function before calling
       # a base output.
+      #
+      # Note that this is applied *before* the base output.
+      # This is different than {SoberSwag::Reporting::Input::Mapped}, which does the reverse.
+      # IE, this class does `call block -> pass result to base output`,
+      # while the other does `call serializer -> pass result to block`.
+      #
+      # If you want to get *really* nerdy, this is *contravariant* to `Mapped`.
+      #
+      # This lets you do things like making an output that serializes to strings via `to_s`:
+      #
+      # ```ruby
+      # ToSTextOutput = SoberSwag::Reporting::Output::ViaMap.new(
+      #   SoberSwag::Reporting::Output.text,
+      #   proc { |arg| arg.to_s }
+      # )
+      #
+      # class Person
+      #   def to_s
+      #     'Person'
+      #   end
+      # end
+      #
+      # ToSTextOutput.call(Person.new) # => 'Person'
+      # ```
       class ViaMap < Base
         def initialize(output, mapper)
           @output = output
