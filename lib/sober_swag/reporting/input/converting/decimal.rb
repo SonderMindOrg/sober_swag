287ad57f697d80724840562889e86f108a3354f1
Anthony Super <asuper@sondermind.com>
2022-07-21 12:46:58 -0600

    Fix Report Problem
    1. Stricter validation on arguments to report
    2. Fixes a bug in decimal report

--- lib/sober_swag/reporting/input/converting/decimal.rb
+++ lib/sober_swag/reporting/input/converting/decimal.rb
@@ -12,7 +12,7 @@ module SoberSwag
           .mapped do |v|
             BigDecimal(v)
           rescue ArgumentError
-            Report::Value.new('was not a decimal')
+            Report::Value.new(['was not a decimal'])
           end).described(<<~MARKDOWN).referenced('SoberSwag.Converting.Decimal')
             Decimal formatted input.
             Will either convert a JSON number to a decimal, or accept a string representation.
